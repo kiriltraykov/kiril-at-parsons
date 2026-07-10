1d9a0a114eb4197b599c93e09c1b943600caf8bf
Kiril Traykov <kiril_traykov@yahoo.com>
2026-07-10 18:07:10 -0400

    Update main.js

--- portfolio/03CraftBeerUniverse/main.js
+++ portfolio/03CraftBeerUniverse/main.js
@@ -3,6 +3,10 @@
 
         $('#skipIntro').on('click', function(){
 
+           if (window.innerWidth <= 768) {
+    $('#description').appendTo('#d3Holder');
+}
+
         $('#statement1').remove();
         $('#li1').remove();
         $('#li2').remove();
