7d287ba34055f197edb4772b27ac908d5b338f8e
Kiril Traykov <kiril_traykov@yahoo.com>
2026-07-12 06:10:18 -0400

    Update main.js

--- portfolio/11AlphabetWings/main.js
+++ portfolio/11AlphabetWings/main.js
@@ -1,30 +1,30 @@
+var isMobile = window.innerWidth <= 768;
 
+$(document).ready(function(){
 
-	$(document).ready(function(){
+    $('.resize').hide();
+    $('h4').hide();
+    $('.shoe').hide();
 
-		$('.resize').hide();
-		$('h4').hide();
-		$('.shoe').hide();
+    $('#button1').on('click', function(){
 
+        if (isMobile) {
+            $('#button1').hide();
+        } else {
+            $('#box1').remove();
+        }
 
-		$('#button1').on('click', function(){
+        $('.resize').show();
+        $('h4').show();
 
-	        $('#box1').remove();
-	        $('.resize').show();
-	        $('h4').show();
+    });
 
-		        $('#button2').on('click', function(){
+    $('#button2').on('click', function(){
 
- 					$('h4').remove();
-		        	$('.shoe').show();
-		        	$('.shoe').css("margin-right","5%");
-   
-      		      });
-   
-      		});
+        $('h4').remove();
+        $('.shoe').show();
+        $('.shoe').css("margin-right","5%");
 
-		});
+    });
 
-
-
-
+});
